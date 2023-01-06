1d7e2a24b82b71fb22b20b20fa221b45ecc18c5e
huynd86 <huynd86@fpt.com.vn>
2023-01-06 11:47:04 +0700

    change syntax in queryset_to_xlsx

--- queryset_to_xlsx.py
+++ queryset_to_xlsx.py
@@ -1,6 +1,7 @@
 from market.models import *
 import pandas as pd
-df = pd.DataFrame(User.objects.all().values_list('id', 'last_login', 'is_superuser', 'first_name', 'last_name', 'is_staff', 'is_active', 'date_joined', 'email', 'phone_number', 'avatar', 'email_verified', 'phone_verified', 'balance', 'is_business'), columns=['id', 'last_login', 'is_superuser', 'first_name', 'last_name', 'is_staff', 'is_active', 'date_joined', 'email', 'phone_number', 'avatar', 'email_verified', 'phone_verified', 'balance', 'is_business']
+df = pd.DataFrame(User.objects.all().values_list('id', 'last_login', 'is_superuser', 'first_name', 'last_name', 'is_staff', 'is_active', 'date_joined', 'email', 'phone_number', 'avatar', 'email_verified', 'phone_verified', 'balance', 'is_business'), columns=['id', 'last_login', 'is_superuser', 'first_name', 'last_name', 'is_staff', 'is_active', 'date_joined', 'email', 'phone_number', 'avatar', 'email_verified', 'phone_verified', 'balance', 'is_business'])
+tz_col = ["last_login", "date_joined"]
 for tzcol in tz_col:
 	df[tzcol] = df[tzcol].apply(lambda r: r.replace(tzinfo=None))
 df.to_excel('output.xlsx')
