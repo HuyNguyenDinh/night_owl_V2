acc6d6bf43214e96fec037785567956e14cb2b95
HuyTur <huyn27316@gmail.com>
2023-02-04 05:51:01 +0000

    Update settings.py

--- night_owl_market/settings.py
+++ night_owl_market/settings.py
@@ -262,8 +262,6 @@ SIMPLE_JWT = {
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
 CSRF_TRUSTED_ORIGINS = ['https://*.nguyendinhhuy.dev','https://*.127.0.0.1', 'http://*.127.0.0.1']
-CSRF_COOKIE_SECURE = True
-SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 
 CELERY_ROUTES = {
