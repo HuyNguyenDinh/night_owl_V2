ade52b16c3286fb1e7920c2494d8aa64b10890ee
DinhHuy <huyn27316@gmail.com>
2023-04-24 14:15:07 +0700

    [+] update phone_verified default value to true for by pass

--- market/models.py
+++ market/models.py
@@ -46,7 +46,7 @@ class User(AbstractUser):
     phone_number = models.CharField(unique=True, blank=False, null=False, max_length=50)
     avatar = models.ImageField(upload_to="upload/%Y/%m", null=True, blank=True)
     email_verified = models.BooleanField(default=False)
-    phone_verified = models.BooleanField(default=False)
+    phone_verified = models.BooleanField(default=True)
     balance = models.DecimalField(decimal_places=2, max_digits=20, default=0.01)
 
     USERNAME_FIELD = "email"
