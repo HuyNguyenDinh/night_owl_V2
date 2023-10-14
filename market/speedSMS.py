de2d45f4f3e37d7be1d7678f60f32a099061dc69
Dinh Huy <huyn27316@gmail.com>
2023-10-14 22:48:37 +0700

    Update speedSMS.py

--- market/speedSMS.py
+++ market/speedSMS.py
@@ -3,7 +3,7 @@ import requests
 endpoint = "https://api.speedsms.vn/index.php/sms/send"
 type = 2
 sender = "9e2e49fdf026beae"
-access_token = "zhu2N-gJRJe6PWdWF_OXfr6-jbNm71gz"
+access_token = "access-token"
 
 def send_sms(receiver, content):
     params = {
